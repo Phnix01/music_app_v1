ec6df54c6f30c0018c9924de656510642c2cb4bd
Phnix01 <fomar9235@gmail.com>
2024-08-25 22:14:01 +0100

    implémentation des méthodes playMusic, _pauseMusic, _stopMusic

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -9,6 +9,72 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> {
+  late AudioPlayer _audioPlayer;
+  bool isPlaying = false;
+  Duration currentPosition = Duration.zero;
+  Duration totalDuration = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+
+    // on va écouter la durée de la piste
+    _audioPlayer.onDurationChanged.listen((duration) {
+      setState(() {
+        totalDuration = duration;
+      });
+
+      // on va écouter ici la position courante
+      _audioPlayer.onPositionChanged.listen((position) {
+        setState(() {
+          currentPosition = position;
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _audioPlayer.dispose();
+  }
+
+  // je vais maintenant  au  codage des méthodes pour contrôle de la track
+
+  /// la méthode playMusic
+  void _playMusic() async {
+    try {
+      await _audioPlayer.play(UrlSource(''));
+      setState(() {
+        isPlaying = true;
+      });
+    } catch (e) {
+      print('error dans $e');
+    }
+  }
+
+// la méthode pause
+  void _pauseMusic() async {
+    try {
+      await _audioPlayer.pause();
+      setState(() {
+        isPlaying = false;
+      });
+    } catch (e) {
+      print('erreur pause $e');
+    }
+  }
+
+  // la méthode stop arreter la music même si je penses qu'elle n'est pas importante
+  void _stopMusic() async {
+    try {
+      await _audioPlayer.stop();
+    } catch (e) {
+      print('erreur stop $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
